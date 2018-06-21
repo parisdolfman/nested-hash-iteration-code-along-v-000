4396b74d2fb832b1067f37240499bbb2b176b3c6
Paris Dolfman <parisdolfman@gmail.com>
2018-06-21 18:16:09 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -4,7 +4,7 @@ require 'pry'
   # contacts = {
   #   "Jon Snow" => {
   #     name: "Jon",
-  #     email: "jon_snow@thewall.we", 
+  #     email: "jon_snow@thewall.we",
   #     favorite_ice_cream_flavors: ["chocolate", "vanilla"]
   #   },
   #   "Freddy Mercury" => {
@@ -13,7 +13,27 @@ require 'pry'
   #     favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
   #   }
   # }
-  
+
 def remove_strawberry(contacts)
+  contacts = {
+     "Jon Snow" => {
+       name: "Jon",
+       email: "jon_snow@thewall.we",
+       favorite_ice_cream_flavors: ["chocolate", "vanilla"]
+     },
+     "Freddy Mercury" => {
+       name: "Freddy",
+       email: "freddy@mercury.com",
+       favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
+     }
+   }
+
+  contacts.each do |person, data|
+    data.each do |key, value|
+      if key == "strawberry"
+      #remove
+    else
+      #continue
+    end
 
 end
